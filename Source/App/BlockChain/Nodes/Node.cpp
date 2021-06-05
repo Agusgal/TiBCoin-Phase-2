a8b56947be48b6b468ceb3b942ca6035c47db991
Agusgal <agaldeman@yahoo.com.ar>
2021-06-05 19:14:44 -0300

    asdasd

---
+++ Source/App/BlockChain/Nodes/Node.cpp
@@ -0,0 +1,33 @@
+
+class Node
+{
+public:
+	void nothing(void);
+
+
+	
+
+private:
+	//void nothing(void);
+	
+
+	//sendGetMEssage....
+	//GetActions;
+
+	//Server server;
+	//Client client;
+};
+
+
+class Full :public Node
+{
+
+
+};
+
+
+class SPV :public Node
+{
+
+
+};
